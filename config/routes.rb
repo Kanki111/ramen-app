468992744d33c9799e00e4d7c5698b79adc763a6
Kanki111 <kanki.m0924@gmail.com>
2020-09-07 23:15:06 +0900

    root

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,23 @@
 Rails.application.routes.draw do
 
   root "member/sessions#new"
-  get "member/images/index" => "images#index"
-  get "login" => "member/sessions#new", as: :login
-  post "session" => "member/sessions#create", as: :session
-  delete "session" => "member/sessions#destroy"
-  resource :password, only: [ :show, :edit, :update]
-  get "member/ramen_member/new" => "member/member#new"
-  post "member/ramen_member/new" => "member/member#create"
-  get "member/ramen_member/show" => "member/member#show"
-  resources :comments, only: [:create]
-  resources :posts do
-    resources :comments, only: [:create]
-  end
 
   namespace :member_posts do
     resources :comments
     post "comment" => "member_post_comment"
   end
+    get "images/index" => "images#index"
+    get "login" => "sessions#new", as: :login
+    post "session" => "sessions#create", as: :session
+    delete "session" => "sessions#destroy"
+    resource :password, only: [ :show, :edit, :update]
+    get "ramen_member/new" => "member#new"
+    post "ramen_member/new" => "member#create"
+    get "ramen_member/show" => "member#show"
+    resources :comments, only: [:create]
+    resources :posts do
+      resources :comments, only: [:create]
+    end
 
   namespace :manager do
     root "top#index"
