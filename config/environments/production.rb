de47811bea8dd53509545dad54f7a3469c0637b3
Kanki111 <kanki.m0924@gmail.com>
2020-09-08 00:31:34 +0900

    web_console

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -115,4 +115,5 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.hosts << "localhost"
   config.hosts << "ramen-app-main.herokuapp.com"
+  config.web_console.whitelisted_ips = [ "172.16.0.0/12" ]
 end
