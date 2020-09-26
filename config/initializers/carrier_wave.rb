6356fa67d50566e342c822bb8e74fa4973d3a3cc
Kanki111 <kanki.m0924@gmail.com>
2020-09-27 07:24:22 +0900

    image

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -8,6 +8,9 @@
         aws_secret_access_key: 'QAwu0CA2xMPpWqdah2m2zjKeQUTNh5d37ZER/vje',
         region: 'ap-northeast-1'
       }
+      config.fog_directory     =  ENV['ramen-app-image']
+      # キャッシュの保存期間
+      config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
     config.fog_directory  = 'ramen-app-image'
     config.asset_host = "https://s3.ap-northeast-1.amazonaws.com/バケット名"
     config.cache_storage = :fog
