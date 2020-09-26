ecfc0bc76ead0581c200d4b894bcfbb16a4e029b
Kanki111 <kanki.m0924@gmail.com>
2020-09-27 07:00:07 +0900

    image

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -5,7 +5,7 @@ if Rails.env.production?
         # Amazon S3用の設定
         :provider              => 'AWS',
         :region                => ENV['ap-northeast-1'],
-        :aws_access_key_id     => ENV['AKIA54NMMBXY7M25GSXI'],
+        :aws_access_key_id     => ENV['AKIA54NMMBXYUJPIEM67'],
         :aws_secret_access_key => ENV['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']
       }
       config.fog_directory     =  ENV['ramen-app-image']
