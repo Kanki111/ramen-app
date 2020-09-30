4db5989d6aa64ef2e90d698e5ccfc22d81e1b67b
Kanki111 <kanki.m0924@gmail.com>
2020-10-01 05:23:44 +0900

    image

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,17 +1,13 @@
-require 'carrierwave/storage/abstract'
-require 'carrierwave/storage/file'
-require 'carrierwave/storage/fog'
+unless Rails.env.development? || Rails.env.test?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      provider: 'AWS',
+      aws_access_key_id: 'AKIA54NMMBXYVN2SN2GO',
+      aws_secret_access_key: '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',
+      region: 'ap-northeast-1'
+    }
 
-CarrierWave.configure do |config|
-  config.storage = :fog
-  config.fog_provider = 'fog/aws'
-  config.fog_credentials = {
-    provider: 'AWS',
-    aws_access_key_id: Rails.application.secrets.AKIA54NMMBXYVN2SN2GO,
-    aws_secret_access_key: Rails.application.k7w7sCBBe8/3pag6NVgRoawEUyRca8X6PP8h7HdT,
-    region: 'ap-northeast-1' 
-  }
-
-  config.fog_directory  = 'image-ramen-app-main'
-  config.asset_host = 'https://s3-ap-northeast1.amazonaws.com/image-ramen-app-main'
+    config.fog_directory  = 'image-ramen-app-main'
+    config.cache_storage = :fog
+  end
 end
