c9d608fd3b8d279a37e967ecf94a0fa2fa69069e
Kanki111 <kanki.m0924@gmail.com>
2020-09-30 17:56:04 +0900

    carrierwave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,20 +2,16 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-if Rails.env.production?
-    CarrierWave.configure do |config|
-      config.fog_provider = 'fog/aws'  #=> 追加
-      config.fog_credentials = {
-        # Amazon S3用の設定
-        :provider              => 'AWS',
-        :region                => ENV['ap-northeast-1'],
-        :aws_access_key_id     => ENV['AKIA54NMMBXYVN2SN2GO'],
-        :aws_secret_access_key => ENV['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']
-      }
-      config.fog_directory     =  ENV['image-ramen-app-main']
-      # キャッシュの保存期間
-      config.fog_attributes = { 'Cache-Control' => "max-age=#{365.day.to_i}" }
-    end
-    # 日本語ファイル名の設定
-    CarrierWave::SanitizedFile.sanitize_regexp = /[^[:word:]\.\-\+]/ 
-  end
+CarrierWave.configure do |config|
+  config.storage = :fog
+  config.fog_provider = 'fog/aws'
+  config.fog_credentials = {
+    provider: 'AWS',
+    aws_access_key_id: Rails.application.secrets.AKIA54NMMBXYVN2SN2GO,
+    aws_secret_access_key: Rails.application.k7w7sCBBe8/3pag6NVgRoawEUyRca8X6PP8h7HdT,
+    region: 'ap-northeast-1' 
+  }
+
+  config.fog_directory = 'https://image-ramen-app-main.s3.amazonaws.com'
+  config.asset_host = 'https://image-ramen-app-main.s3.amazonaws.com'
+end
