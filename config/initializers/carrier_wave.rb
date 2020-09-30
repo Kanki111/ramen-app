7db3ab0d1f21af822732e3e67c277b2a47c9cbd3
Kanki111 <kanki.m0924@gmail.com>
2020-10-01 06:15:24 +0900

    image

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -1,13 +1,14 @@
-unless Rails.env.development? || Rails.env.test?
-  CarrierWave.configure do |config|
-    config.fog_credentials = {
-      provider: 'AWS',
-      aws_access_key_id: 'AKIA54NMMBXYVN2SN2GO',
-      aws_secret_access_key: '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',
-      region: 'ap-northeast-1'
-    }
+config/carrier_wave.rb
 
-    config.fog_directory  = 'image-ramen-app-main'
-    config.cache_storage = :fog
-  end
+if Rails.env.production?
+    CarrierWave.configure do |config|
+      config.fog_credentials = {
+        # Amazon S3用の設定
+        :provider              => 'AWS',
+        :region                => ENV['ap-northeast-1'],   
+        :aws_access_key_id     => ENV['AKIA54NMMBXYVN2SN2GO'],
+        :aws_secret_access_key => ENV['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']
+      }
+      config.fog_directory     =  ENV['image-ramen-app-main']
+    end
 end
