e17545d1430a2c0f03a41f525fa46e553eabd5bd
Kanki111 <kanki.m0924@gmail.com>
2020-11-13 22:52:46 +0900

    readmeの変更

--- app/models/member/post.rb
+++ app/models/member/post.rb
@@ -6,4 +6,6 @@ class Member::Post < ApplicationRecord
     has_rich_text :content
     has_one :spot, dependent: :destroy
     accepts_nested_attributes_for :spot
+    geocoded_by :place
+  after_validation :geocode, if: :address_changed?
 end
